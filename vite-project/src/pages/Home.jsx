2d7cf4b9fda63f0ead06691f8d7cb44ef96a62c9
Gustavo Fernandez Pascoaleto <gusalot22@gmail.com>
2024-09-04 00:28:28 -0300

    FEATURE: Replace home

--- vite-project/src/pages/Home.jsx
+++ vite-project/src/pages/Home.jsx
@@ -1,7 +1,10 @@
 import { Link } from 'react-router-dom'
 
 import styles from './Home.module.css';
-import sojaImage from '../assets/soja.webp';
+import livreImage from '../assets/livre.jpeg';
+import talhaoImage from '../assets/talhao.png'
+import tratorImage from '../assets/trator.webp'
+
 
 
 
@@ -10,15 +13,15 @@ const Home = () => {
 
     <div className={styles.container}>
 
-      <Link to='/mapalivre' className={styles.block} style={{ backgroundImage: `url(${sojaImage})` }}>
+      <Link to='/mapalivre' className={styles.block} style={{ backgroundImage: `url(${livreImage})` }}>
         <h1>Mapa Livre</h1>
       </Link>
 
-      <Link to='/telapropriedade' className={styles.block} style={{ backgroundImage: `url(${sojaImage})` }}>
+      <Link to='/telapropriedade' className={styles.block} style={{ backgroundImage: `url(${tratorImage})` }}>
         <h1>Propriedeades e culturas</h1>
       </Link>
 
-      <Link to='/add-talhao' className={styles.block} style={{ backgroundImage: `url(${sojaImage})` }}>
+      <Link to='/add-talhao' className={styles.block} style={{ backgroundImage: `url(${talhaoImage})` }}>
         <h1>Adicionar Talhões</h1>
       </Link>
 
