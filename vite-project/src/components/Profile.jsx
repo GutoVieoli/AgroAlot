56ef091d69ebbdb0eff584b898bca6fcad0ec240
GustavoAlot <gusalot22@gmail.com>
2024-09-12 16:11:57 -0300

    FEATURE: Front meuperfil e tela de edicao do perfil

--- vite-project/src/components/Profile.jsx
+++ vite-project/src/components/Profile.jsx
@@ -1,17 +1,17 @@
 import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom'; // Adicionar o useNavigate
 import './Profile.css';
 import Perfil from '../assets/Perfil.svg';
 
 const Profile = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
     const [userData, setUserData] = useState({ name: '', email: '' });
+    const navigate = useNavigate(); // Navegação
 
-    
     useEffect(() => {
-        
         const fetchUserData = async () => {
             try {
-                const response = await fetch('http://localhost:3000/user'); // back
+                const response = await fetch('http://localhost:3000/user');
                 const data = await response.json();
                 setUserData({ name: data.name, email: data.email });
             } catch (error) {
@@ -19,7 +19,7 @@ const Profile = () => {
             }
         };
 
-        fetchUserData(); 
+        fetchUserData();
     }, []);
 
     const toggleMenu = () => {
@@ -27,21 +27,25 @@ const Profile = () => {
     };
 
     const handleLogout = () => {
-        // logica do logout
-       
+        console.log("Usuário deslogado");
+        window.location.href = '/login';
+    };
+
+    const handleEditAccount = () => {
+        navigate('/edit-account'); // Redireciona para a página de edição
     };
 
     return (
         <div className="profile-container" onClick={toggleMenu}>
             <img src={Perfil} alt="Perfil do Usuário" className="profile-pic" />
-            
+
             {isMenuOpen && (
                 <div className="profile-menu">
                     <ul>
                         <li>Nome: {userData.name}</li>
                         <li>Email: {userData.email}</li>
                         <li>
-                            <button className="edit-account-btn">Editar Conta</button>
+                            <button className="edit-account-btn" onClick={handleEditAccount}>Editar Conta</button>
                         </li>
                         <li>
                             <button className="logout-btn" onClick={handleLogout}>Logout</button>
